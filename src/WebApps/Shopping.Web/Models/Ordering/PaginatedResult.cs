119e35ad239bce36dbe98a76be31c63b2cbc6f00
Marcio de Assis Martins <marcio.martins@hiplatform.com>
2024-08-06 22:06:16 -0300

    Complementação e desenvolvimento de WebApps

---
+++ src/WebApps/Shopping.Web/Models/Ordering/PaginatedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Web.Models.Ordering;
+
+public class PaginatedResult<TEntity>(int pageIndex, int pagesize, long count, IEnumerable<TEntity> data)
+    where TEntity : class 
+{
+    public int PageIndex { get; } = pageIndex;
+    public int PageSize { get; } = pagesize;
+    public long Count { get; } = count;
+    public IEnumerable<TEntity> Data { get; } = data;
+}
