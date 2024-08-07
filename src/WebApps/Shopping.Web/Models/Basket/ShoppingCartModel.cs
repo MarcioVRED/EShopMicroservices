244189bdbe7692ec40d88c954f3f8d8d7d5e28e2
Marcio de Assis Martins <marcio.martins@hiplatform.com>
2024-08-07 18:24:42 -0300

    Commit final do projeto

--- src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -13,7 +13,7 @@ public class ShoppingCartItemModel
     public string Color { get; set; } = default!;
     public decimal Price { get; set; } = default!;
     public Guid ProductId { get; set; } = default!;
-    public int MyProperty { get; set; } = default!;
+    public string ProductName { get; set; } = default!;
 }
 
 // wrapper classes
